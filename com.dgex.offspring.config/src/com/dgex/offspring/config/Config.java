02fd14a28065f7d14be077510c6af38fd6f9d678
Dirk Dotcom <incentivetoken@gmail.com>
2014-05-12 19:55:36 +0200

    Version bump to 0.4.5

--- com.dgex.offspring.config/src/com/dgex/offspring/config/Config.java
+++ com.dgex.offspring.config/src/com/dgex/offspring/config/Config.java
@@ -15,7 +15,7 @@ import org.eclipse.swt.widgets.Display;
 
 public class Config {
 
-  public static final String VERSION = "0.4.4";
+  public static final String VERSION = "0.4.5";
   public static final String TITLE = "Offspring v" + VERSION;
 
   static Logger logger = Logger.getLogger(Config.class);
