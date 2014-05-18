3af4ca543230a31b8139450c92a841002657bfe2
Dirk Dotcom <incentivetoken@gmail.com>
2014-05-18 13:34:58 +0200

    Revert "Use different ports for test net"
    This reverts commit 99803a4d8b9033b3f829055b4a0c37e0e5278e3e.

--- com.dgex.offspring.nxtCore/nxt/src/java/nxt/http/API.java
+++ com.dgex.offspring.nxtCore/nxt/src/java/nxt/http/API.java
@@ -27,7 +27,7 @@ import java.util.Set;
 
 public final class API {
 
-    private static final int TESTNET_API_PORT = 6886;
+    private static final int TESTNET_API_PORT = 6876;
 
     static final Set<String> allowedBotHosts;
 
